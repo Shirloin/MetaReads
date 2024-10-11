d5ea0d9f7b9646d585672e1f5d498d791c4be1b6
shirloin <cx@Riccardo.>
2024-10-11 18:21:23 +0700

    add: browser router

---
+++ src/MetaReads_frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/AppRouter';
+// import { MetaReads_backend } from 'declarations/MetaReads_backend';
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App;
