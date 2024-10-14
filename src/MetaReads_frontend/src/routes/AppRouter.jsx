6defd2feda0277e1a5040c677eb38773d358a3a7
Valentino Setiawan <valentinosetiawan32@gmail.com>
2024-10-14 11:17:54 +0700

    Migrate to JSX

--- src/MetaReads_frontend/src/routes/AppRouter.jsx
+++ src/MetaReads_frontend/src/routes/AppRouter.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import DebugPage from "../page/DebugPage";
 import LoginPage from "../page/LoginPage";
+import HomePage from "../page/HomePage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
             {
                 path: "/debug",
                 element: <DebugPage />
