a5d1c20ea4b6855f8293dcd7dba8dc4a423c8074
Valentino Setiawan <valentinosetiawan32@gmail.com>
2024-10-14 18:15:04 +0700

    Add Top navbar profile

--- src/MetaReads_frontend/src/components/Layout/BaseLayout.jsx
+++ src/MetaReads_frontend/src/components/Layout/BaseLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function BaseLayout() {
   return (
     <>
-      <div className="min-h-screen w-full bg-black">
+      <div className="font-montserrat min-h-screen w-full bg-black">
         <Outlet />
       </div>
     </>
--- src/MetaReads_frontend/src/components/Navbar/TopNavbar.jsx
+++ src/MetaReads_frontend/src/components/Navbar/TopNavbar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import SearchBar from "../Form/Input/TextField/SearchBar";
+import NavbarProfile from "../Profile/NavbarProfile";
+import { useCollapsed } from "../../lib/collapsed_provider";
 
 export default function TopNavbar() {
+  const { collapsed } = useCollapsed();
+  const [query, setQuery] = useState("");
+
   return (
     <div
       className="fixed h-[100px] w-full text-white"
@@ -12,15 +18,21 @@ export default function TopNavbar() {
         className="absolute inset-0"
         style={{
           backgroundColor: "#14181E",
-          opacity: "0.71",
-          zIndex: -1,
+          opacity: "0.11",
+          zIndex: 1,
         }}
       ></div>
-      <div className="relative z-10 flex h-full w-full items-center justify-around gap-3 px-4">
-        <div className="w-10/12">
-          <SearchBar />
+      <div className="relative z-10 flex h-full w-full items-center gap-2 px-4">
+        <div
+          className="w-full flex-grow transition-all duration-300"
+          style={{ maxWidth: collapsed ? "85%" : "76%" }}
+        >
+          <SearchBar query={query} setQuery={setQuery} />
         </div>
-        <div className="w-2/12">Profile here</div>
+
+        <div className="">
+          <NavbarProfile />
+        </div>
       </div>
     </div>
   );
