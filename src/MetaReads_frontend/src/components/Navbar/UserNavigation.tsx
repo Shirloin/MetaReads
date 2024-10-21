90eeb7b53fe8670326a92dceee36d323a448aaaf
Valentino Setiawan <valentinosetiawan32@gmail.com>
2024-10-21 14:33:28 +0700

    fix(sidenav): Fix sidenav redirect link

--- src/MetaReads_frontend/src/components/Navbar/UserNavigation.tsx
+++ src/MetaReads_frontend/src/components/Navbar/UserNavigation.tsx
@@ -64,7 +64,7 @@ export default function UserNavigation() {
             }}
           />
         }
-        component={<Link to={createUrl("/")} />}
+        component={<Link to={createUrl("/store")} />}
       >
         Store
       </MenuItem>
