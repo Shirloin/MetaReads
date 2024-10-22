3d50a928378e6c6d2a0889252d35a1d47acc9e6d
Valentino Setiawan <valentinosetiawan32@gmail.com>
2024-10-22 09:30:01 +0700

    style(lit-up-button): update style to match subscribe button

--- src/MetaReads_frontend/src/components/Form/Button/LitUpButton.tsx
+++ src/MetaReads_frontend/src/components/Form/Button/LitUpButton.tsx
@@ -2,7 +2,7 @@ export default function LitupButton({ text, className, }: { text?: string, class
     return (
         <button className="p-[3px] relative">
             <div className="absolute inset-0 bg-gradient-to-r from-[#efaf21] to-[#efaf21] rounded-lg" />
-            <div className={`px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white uppercase ${className}`}>
+            <div className={`px-6 py-3  bg-black rounded-[6px]  relative group transition duration-200 text-white uppercase ${className}`}>
                 {text}
             </div>
         </button >
