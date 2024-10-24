24866c05030054029413c3006cded0c2afd6f3bd
Vasang123 <valentinosetiawan32@gmail.com>
2024-10-24 21:43:02 +0700

    refactor(readpage): split header and view

--- src/MetaReads_frontend/src/page/ReadPage.tsx
+++ src/MetaReads_frontend/src/page/ReadPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
@@ -9,24 +9,16 @@ import { highlightPlugin, Trigger } from "@react-pdf-viewer/highlight";
 import "@react-pdf-viewer/zoom/lib/styles/index.css";
 import "@react-pdf-viewer/page-navigation/lib/styles/index.css";
 import "@react-pdf-viewer/highlight/lib/styles/index.css";
-import {
-  FullScreenIcon,
-  fullScreenPlugin,
-  RenderEnterFullScreenProps,
-} from "@react-pdf-viewer/full-screen";
+import { fullScreenPlugin } from "@react-pdf-viewer/full-screen";
 import "@react-pdf-viewer/full-screen/lib/styles/index.css";
-import {
-  RenderShowSearchPopoverProps,
-  searchPlugin,
-} from "@react-pdf-viewer/search";
-import { SearchIcon } from "@react-pdf-viewer/search";
+import { searchPlugin } from "@react-pdf-viewer/search";
+
 import "@react-pdf-viewer/search/lib/styles/index.css";
-import InputField from "../components/Form/Input/TextField/InputField";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Header } from "../components/PDFReader/Header";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`;
 
-export default function ReadPage() {
+const ReadPage = () => {
   const [selectedText, setSelectedText] = useState<string | undefined>();
   const [pageInput, setPageInput] = useState<number>(1);
   const [zoomLevel, setZoomLevel] = useState<number>(1);
@@ -35,7 +27,6 @@ export default function ReadPage() {
   const fullScreenPluginInstance = fullScreenPlugin();
   const pageNavigationPluginInstance = pageNavigationPlugin();
   const zoomPluginInstance = zoomPlugin({ enableShortcuts: false });
-  const zoomLevels = [0.5, 1, 1.5, 2, 2.5, 3];
 
   const handleZoom = (scale: number) => {
     setZoomLevel(scale);
@@ -47,6 +38,7 @@ export default function ReadPage() {
       handleZoom(1);
     }
   }, [isDocumentLoaded]);
+
   useEffect(() => {
     console.log(selectedText);
   }, [selectedText]);
@@ -112,78 +104,16 @@ export default function ReadPage() {
 
   return (
     <div className="flex h-screen flex-col text-white">
-      <div className="flex justify-between px-4">
-        <div className="flex items-center justify-center gap-2">
-          <div className="flex items-center justify-center rounded-md bg-gray-800 p-3 px-3">
-            <EnterFullScreen>
-              {(props: RenderEnterFullScreenProps) => (
-                <button onClick={props.onClick} className="text-white">
-                  <FullScreenIcon />
-                </button>
-              )}
-            </EnterFullScreen>
-          </div>
-          <div className="flex items-center justify-center rounded-md bg-gray-800 p-2 px-3">
-            <ShowSearchPopover>
-              {(props: RenderShowSearchPopoverProps) => (
-                <button onClick={props.onClick} className="text-white">
-                  <SearchIcon />
-                </button>
-              )}
-            </ShowSearchPopover>
-          </div>
-        </div>
-        <div className="my-2 flex justify-center gap-2">
-          <button
-            onClick={jumpToPreviousPage}
-            className="w-[80px] rounded bg-gray-800 p-2 text-white"
-          >
-            Previous
-          </button>
-
-          <div className="flex items-center">
-            <CurrentPageLabel>
-              {({ currentPage, numberOfPages }) => (
-                <span className="flex justify-center gap-2">
-                  <div className="flex w-[80px]">
-                    <InputField
-                      label={"Page"}
-                      value={currentPage.toString()}
-                      onChange={handlePageInputChange}
-                      type="number"
-                    />
-                  </div>
-                  <div className="flex items-center">/ {numberOfPages}</div>
-                </span>
-              )}
-            </CurrentPageLabel>
-          </div>
-          <button
-            onClick={jumpToNextPage}
-            className="w-[80px] rounded bg-gray-800 p-2 text-white"
-          >
-            Next
-          </button>
-        </div>
-        <div className="mx-2 flex items-center">
-          <div className="flex items-center">
-            <FormControl size="small">
-              <InputLabel id="demo-simple-select-helper-label">Zoom</InputLabel>
-              <Select
-                value={zoomLevel}
-                onChange={(e: any) => handleZoom(Number(e.target.value))}
-              >
-                {zoomLevels.map((scale) => (
-                  <MenuItem key={scale} value={scale}>
-                    {scale === 1 ? "100%" : `${scale * 100}%`}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </div>
-        </div>
-      </div>
-
+      <Header
+        jumpToNextPage={jumpToNextPage}
+        jumpToPreviousPage={jumpToPreviousPage}
+        CurrentPageLabel={CurrentPageLabel}
+        handlePageInputChange={handlePageInputChange}
+        zoomLevel={zoomLevel}
+        handleZoom={handleZoom}
+        ShowSearchPopover={ShowSearchPopover}
+        EnterFullScreen={EnterFullScreen}
+      />
       <div className="flex-grow overflow-auto">
         <Worker
           workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
@@ -204,4 +134,6 @@ export default function ReadPage() {
       </div>
     </div>
   );
-}
+};
+
+export default ReadPage;
